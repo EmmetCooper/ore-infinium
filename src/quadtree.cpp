8d3813f1de5d3197b7a9c737700e13cb6cfafb42
Shaun Reich <sreich@kde.org>
2013-04-18 13:18:17 -0400

    add license header, code style fixes in quadtree

--- src/quadtree.cpp
+++ src/quadtree.cpp
@@ -1,80 +1,127 @@
+/******************************************************************************
+ *   Copyright (C) 2013 by Shaun Reich <sreich@kde.org>                       *
+ *                                                                            *
+ *   This program is free software; you can redistribute it and/or            *
+ *   modify it under the terms of the GNU General Public License as           *
+ *   published by the Free Software Foundation; either version 2 of           *
+ *   the License, or (at your option) any later version.                      *
+ *                                                                            *
+ *   This program is distributed in the hope that it will be useful,          *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of           *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            *
+ *   GNU General Public License for more details.                             *
+ *                                                                            *
+ *   You should have received a copy of the GNU General Public License        *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.    *
+ *****************************************************************************/
+
 #include "quadtree.h"
 
 #include <assert.h>
 
 Quadtree::Quadtree(XY _center, XY _halfDimension)
-  : boundary(_center, _halfDimension), nodeCapacity(4)
+    : boundary(_center, _halfDimension),
+      nodeCapacity(4)
 {
-  NW = NE = SW = SE = NULL;
-  points.reserve(nodeCapacity);
+    NW = NE = SW = SE = nullptr;
+    points.reserve(nodeCapacity);
 }
 
 Quadtree::Quadtree(XY _center, XY _halfDimension, int _nodeCapacity)
-  : boundary(_center, _halfDimension), nodeCapacity(_nodeCapacity)
+    : boundary(_center, _halfDimension),
+      nodeCapacity(_nodeCapacity)
 {
-  NW = NE = SW = SE = NULL;
-  points.reserve(nodeCapacity);
+    NW = NE = SW = SE = nullptr;
+    points.reserve(nodeCapacity);
 }
 
-bool Quadtree::insert(Item * a) {
-  if (!boundary.containsPoint(a))
-    return false;
+bool Quadtree::insert(Item * a)
+{
+    if (!boundary.containsPoint(a)) {
+        return false;
+    }
 
-  if (points.size() < nodeCapacity) {
-    points.push_back(a);
-    return true;
-  }
+    if (points.size() < nodeCapacity) {
+        points.push_back(a);
+        return true;
+    }
 
-  if (NW == NULL) subdivide();
+    if (NW == nullptr) {
+        subdivide();
+    }
 
-  if (NW->insert(a)) return true;
-  if (NE->insert(a)) return true;
-  if (SW->insert(a)) return true;
-  if (SE->insert(a)) return true;
+    if (NW->insert(a)) {
+        return true;
+    }
 
-  assert(false);
-  return false; // should never happen
+    if (NE->insert(a)) {
+        return true;
+    }
+
+    if (SW->insert(a)) {
+        return true;
+    }
+
+    if (SE->insert(a)) {
+        return true;
+    }
+
+    assert(false);
+    return false; // should never happen
 }
 
 void Quadtree::subdivide() {
-  XY center = boundary.center;
-  XY newDim(boundary.halfDimension.x / 2, boundary.halfDimension.y / 2);
+    XY center = boundary.center;
+    XY newDim(boundary.halfDimension.x / 2, boundary.halfDimension.y / 2);
 
-  NW = new Quadtree(XY(center.x - newDim.x, center.y - newDim.y), newDim);
-  NE = new Quadtree(XY(center.x + newDim.x, center.y - newDim.y), newDim);
-  SW = new Quadtree(XY(center.x - newDim.x, center.y + newDim.y), newDim);
-  SE = new Quadtree(XY(center.x + newDim.x, center.y + newDim.y), newDim);
+    NW = new Quadtree(XY(center.x - newDim.x, center.y - newDim.y), newDim);
+    NE = new Quadtree(XY(center.x + newDim.x, center.y - newDim.y), newDim);
+    SW = new Quadtree(XY(center.x - newDim.x, center.y + newDim.y), newDim);
+    SE = new Quadtree(XY(center.x + newDim.x, center.y + newDim.y), newDim);
 }
 
 void Quadtree::queryRange(std::vector<Item*> & list, AABB range) {
-  if (!boundary.intersectsAABB(range)) return ; // list is empty
+    if (!boundary.intersectsAABB(range)) {
+        return ; // list is empty
+    }
 
-  for (int i = 0; i < points.size(); ++i)
-    if (range.containsPoint(points[i]))
-      list.push_back(points[i]);
+    for (int i = 0; i < points.size(); ++i) {
+        if (range.containsPoint(points[i])) {
+            list.push_back(points[i]);
+        }
+    }
 
-  if (NW == NULL) return ;
-  NW->queryRange(list, range);
-  NE->queryRange(list, range);
-  SW->queryRange(list, range);
-  SE->queryRange(list, range);
+    if (NW == nullptr) {
+        return ;
+    }
+
+    NW->queryRange(list, range);
+    NE->queryRange(list, range);
+    SW->queryRange(list, range);
+    SE->queryRange(list, range);
 }
 
 // scan the tree and remove all node/Item*
 void Quadtree::clear() {
-  if (this == NULL) return ;
-  points.clear();
+    if (this == nullptr) {
+        return ;
+    }
 
-  NW->clear();
-  delete NW;
-  NW = NULL;
-  NE->clear();
-  delete NE;
-  NE = NULL;
-  SW->clear();
-  delete SW;
-  SW = NULL;
-  SE->clear();
-  delete SE;
-  SE = NULL;
+    points.clear();
+
+    NW->clear();
+    delete NW;
+    NW = nullptr;
+
+    NE->clear();
+    delete NE;
+    NE = nullptr;
+
+    SW->clear();
+    delete SW;
+    SW = nullptr;
+
+    SE->clear();
+    delete SE;
+    SE = nullptr;
 }
--- src/quadtree.h
+++ src/quadtree.h
@@ -1,56 +1,73 @@
+/******************************************************************************
+ *   Copyright (C) 2013 by Shaun Reich <sreich@kde.org>                       *
+ *                                                                            *
+ *   This program is free software; you can redistribute it and/or            *
+ *   modify it under the terms of the GNU General Public License as           *
+ *   published by the Free Software Foundation; either version 2 of           *
+ *   the License, or (at your option) any later version.                      *
+ *                                                                            *
+ *   This program is distributed in the hope that it will be useful,          *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of           *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            *
+ *   GNU General Public License for more details.                             *
+ *                                                                            *
+ *   You should have received a copy of the GNU General Public License        *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.    *
+ *****************************************************************************/
+
 #ifndef __QUADTREE__
 #define __QUADTREE__
 
 #include "src/entity.h"
 
 struct XY {
-  XY(float _x, float _y) : x(_x), y(_y) {}
-  float x, y;
+    XY(float _x, float _y) : x(_x), y(_y) {}
+    float x, y;
 };
 
 struct AABB {
-  AABB(XY _center, XY _halfDimension) : center(_center), halfDimension(_halfDimension) {}
+    AABB(XY _center, XY _halfDimension) : center(_center), halfDimension(_halfDimension) {}
 
-  bool containsPoint(Item* item) {
-    XY p(item->x, item->y);
-    return ((p.x > center.x - halfDimension.x && p.x <= center.x + halfDimension.x) &&
-            (p.y > center.y - halfDimension.y && p.y <= center.y + halfDimension.y));
-  }
+    bool containsPoint(Item* item) {
+        XY p(item->x, item->y);
+        return ((p.x > center.x - halfDimension.x && p.x <= center.x + halfDimension.x) &&
+                (p.y > center.y - halfDimension.y && p.y <= center.y + halfDimension.y));
+    }
 
-  bool intersectsAABB(AABB other) {
-    return (((other.center.x - other.halfDimension.x > center.x - halfDimension.x && other.center.x - other.halfDimension.x < center.x + halfDimension.x) ||
-             (other.center.x + other.halfDimension.x > center.x - halfDimension.x && other.center.x + other.halfDimension.x < center.x + halfDimension.x)) &&
-            ((other.center.y - other.halfDimension.y > center.y - halfDimension.y && other.center.y - other.halfDimension.y < center.y + halfDimension.y) ||
-             (other.center.y + other.halfDimension.y > center.y - halfDimension.y && other.center.y + other.halfDimension.y < center.y + halfDimension.y)));
-  }
+    bool intersectsAABB(AABB other) {
+        return (((other.center.x - other.halfDimension.x > center.x - halfDimension.x && other.center.x - other.halfDimension.x < center.x + halfDimension.x) ||
+                 (other.center.x + other.halfDimension.x > center.x - halfDimension.x && other.center.x + other.halfDimension.x < center.x + halfDimension.x)) &&
+                ((other.center.y - other.halfDimension.y > center.y - halfDimension.y && other.center.y - other.halfDimension.y < center.y + halfDimension.y) ||
+                 (other.center.y + other.halfDimension.y > center.y - halfDimension.y && other.center.y + other.halfDimension.y < center.y + halfDimension.y)));
+    }
 
-  XY center;
-  XY halfDimension;
+    XY center;
+    XY halfDimension;
 };
 
 class Quadtree {
- public:
-  Quadtree(XY, XY);
-  Quadtree(XY, XY, int);
+public:
+    Quadtree(XY, XY);
+    Quadtree(XY, XY, int);
 
-  bool insert(Entity *);
-  void subdivide();
-  void queryRange(std::vector<Entity*> &, AABB);
+    bool insert(Entity *);
+    void subdivide();
+    void queryRange(std::vector<Entity*> &, AABB);
 
-  void clear();
-  bool getCollision(std::vector<Entity*>);
+    void clear();
+    bool getCollision(std::vector<Entity*>);
 
-  AABB boundary;
-  int nodeCapacity;
+    AABB boundary;
+    int nodeCapacity;
 
-  // leaves
-  Quadtree * NW;
-  Quadtree * NE;
-  Quadtree * SW;
-  Quadtree * SE;
+    // leaves
+    Quadtree * NW;
+    Quadtree * NE;
+    Quadtree * SW;
+    Quadtree * SE;
 
-  // data
-  std::vector<Entity*> points;
+    // data
+    std::vector<Entity*> points;
 };
 
 #endif // __QUADTREE__
