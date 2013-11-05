4d03901e0e59084b1c3e2405697cc008e945ca9c
Shaun Reich <sreich@kde.org>
2013-11-05 17:04:38 -0500

    set duplicate peers limit on server to prevent (DoS) SYN attacks
    this may need increased if several people want to play the game behind
    the same router...(e.g. family)
    
    but i guess i'll worry about that when i get > 0 other people "playing"
    the "game" ;p

--- src/server/server.cpp
+++ src/server/server.cpp
@@ -77,6 +77,8 @@ void Server::init(uint8_t maxClients, uint16_t port, Client* client)
                                 0 /* assume any amount of incoming bandwidth */,
                                 0 /* assume any amount of outgoing bandwidth */);
 
+    m_server->duplicatePeers = 4;
+
    Debug::assertf(m_server, "failed to create ENet server");
 
     m_world = new World(nullptr, nullptr, this);
